f8ea63a7b78cb281c7ffcd4cb9f76831dfce63e9
katjam <katjamordaunt@gmail.com>
2021-10-23 17:30:54 +0100

    feat: Get json card source from query string

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-'use strict';
-require('./index.html');
-require('./styles/style.scss');
+'use strict'
+require('./index.html')
+require('./styles/style.scss')
 
-const { Elm } = require('./Main.elm');
+const { Elm } = require('./Main.elm')
 
-const cardSet = require('./json/set1.json')
+const urlParams = new URLSearchParams(window.location.search)
+const setName = urlParams.get('setName') ? urlParams.get('setName') : 'set1'
+const cardSet = require('./json/' + setName + '.json')
 
-Elm.Main.init({flags: cardSet});
+Elm.Main.init({flags: cardSet})
